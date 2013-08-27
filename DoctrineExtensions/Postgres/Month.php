ce6429c6f2df1ffbd46d9bf03391ea3422f4117b
Tyler Hand <thand@mesd.k12.or.us>
2013-08-27 14:37:26 -0700

    More functions for various date units

---
+++ DoctrineExtensions/Postgres/Month.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MESD\DoctrineExtensions\DateFunctionsBundle\DoctrineExtensions\Postgres;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+/**
+ *  ExtractMonthFunction ::=
+ *      "MONTH" "(" ArthimeticPrimary ")"
+ *
+ */
+
+class Month extends FunctionNode
+{
+    public $date;
+
+    /**
+     *  @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker) {
+        return "extract(month from " . $sqlWalker->walkArithmeticPrimary($this->date) . ")";
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser) {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->date = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
---
+++ DoctrineExtensions/Postgres/Second.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace MESD\DoctrineExtensions\DateFunctionsBundle\DoctrineExtensions\Postgres;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+/**
+ *  ExtractSecondFunction ::=
+ *      "SECOND" "(" ArthimeticPrimary ")"
+ *
+ */
+
+class Second extends FunctionNode
+{
+    public $date;
+
+    /**
+     *  @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker) {
+        return "extract(second from " . $sqlWalker->walkArithmeticPrimary($this->date) . ")";
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser) {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->date = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
