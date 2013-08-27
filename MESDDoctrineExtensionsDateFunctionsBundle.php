9ae22046ebee65fa207de6ab20d91395c70ca5b5
Tyler Hand <thand@mesd.k12.or.us>
2013-08-27 08:45:39 -0700

    Started Date Functions Bundle

---
+++ MESDDoctrineExtensionsDateFunctionsBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace MESD\DoctrineExtensions\DateFunctionsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MESDDoctrineExtensionsDateFunctionsBundle extends Bundle
+{
+}
