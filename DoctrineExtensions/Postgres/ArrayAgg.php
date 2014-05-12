f558e7a173fa73b877facf43cbd1059cf50c06c6
lighthart <dlighthart@mesd.k12.or.us>
2014-05-12 12:00:40 -0700

    Array_agg

---
+++ DoctrineExtensions/Postgres/ArrayAgg.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace MESD\DoctrineExtensions\DateFunctionsBundle\DoctrineExtensions\Postgres;
+
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+
+/**
+ *
+ */
+
+class ArrayAgg extends FunctionNode
+{
+    public $field;
+    /**
+     *  @override
+     */
+    public function getSql(\Doctrine\ORM\Query\SqlWalker $sqlWalker) {
+        return "array_agg( " . $sqlWalker->walkArithmeticPrimary($this->field) . ")";
+    }
+
+    public function parse(\Doctrine\ORM\Query\Parser $parser) {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+
+        $this->field = $parser->ArithmeticPrimary();
+
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+}
